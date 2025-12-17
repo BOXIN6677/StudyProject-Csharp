b7b8e3f60f5de188273e8e261e155d53298f0363
BOXIN6677 <1619144259@qq.com>
2025-12-17 18:28:57 +0800

    Add files via upload

---
+++ 循环语句.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Specialized;
+
+class Prom
+{
+    static void Main()
+    {
+        Console.WriteLine("============");
+        Console.WriteLine("While循环");
+
+        Console.WriteLine("请输入你的年龄:");
+        int age = int.Parse(Console.ReadLine());
+        while(age <= 18)
+        {
+            Console.WriteLine("你长到了" + age + "岁");
+            age++;
+        }
+        Console.WriteLine("你成年了");
+
+
+
+        Console.WriteLine("============");
+        Console.WriteLine("For循环");
+
+        age = 0;
+        for(int i = age;i <= 18;i++)
+        {
+            Console.WriteLine("你现在" + i + "岁");
+        }
+        Console.WriteLine("你成年了");
+    }
+}
